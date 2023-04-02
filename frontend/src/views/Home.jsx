5d5a309cc6c6dd6fd7062b624f59698fd0c7dba7
Sierra Lopez <sierralopez1006@gmail.com>
2023-04-02 12:58:59 -0700

    sign up viewpage and some css styling elsewhere

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -12,7 +12,7 @@ function Homepage (props){
                 <img className="home-img" src="https://images.unsplash.com/photo-1488646953014-85cb44e25828?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80" alt="travel"/>
                 <SearchBar/>
             </div>
-            <div className='grid'>
+            <div className='grid justify-content-center'>
                 <Cards/>
                 <Cards/>
                 <Cards/>
