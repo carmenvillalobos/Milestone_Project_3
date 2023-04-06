ec942ac2456b73cd3ea8dc136869dd511199fcc4
Sierra Lopez <sierralopez1006@gmail.com>
2023-04-06 12:58:51 -0700

    homepage pics

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -4,6 +4,7 @@ import NavBar from "../components/NavBar"
 import SearchBar from '../components/SearchBar'
 import Cards from '../components/Cards'
 import Button from "react-bootstrap/Button"
+import Card from 'react-bootstrap/Card'
 
 
 function Homepage (props){
@@ -15,10 +16,38 @@ function Homepage (props){
                 <SearchBar/>
             </div>
             <div className='grid justify-content-center'>
-                <Cards/>
-                <Cards/>
-                <Cards/>
-                <Cards/>
+                <div className="grid">
+                    <Card className="box boxes">
+                        <Card.Img src='https://images.unsplash.com/photo-1488747279002-c8523379faaa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fGxhbmRtYXJrc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60' alt="london" className="card-img"/>
+                        <Card.Body>
+                            <Card.Text className="Title">Sightseeing</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="grid">
+                    <Card className="box boxes">
+                        <Card.Img src='https://images.unsplash.com/photo-1624128082323-beb6b8b508db?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c3BhbmlzaCUyMGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60' alt="cliffside picnic" className="card-img"/>
+                        <Card.Body>
+                            <Card.Text className="Title">Cultural staples & delicacies</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="grid">
+                    <Card className="box boxes">
+                        <Card.Img src='https://images.unsplash.com/photo-1662128400928-0e9a410caed7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YmVhY2glMjBhY3Rpdml0aWVzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt="beach boy" className="card-img"/>
+                        <Card.Body>
+                            <Card.Text className="Title">Rest & relaxation</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="grid">
+                        <Card className="box boxes">
+                            <Card.Img src='https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aG90ZWwlMjBhY2NvbW9kYXRpb258ZW58MHx8MHx8&auto=format&fit=crop&w=900&q=60' alt="hotel room" className="card-img"/>
+                            <Card.Body>
+                                <Card.Text className="Title">Affordable accommodations & more</Card.Text>
+                            </Card.Body>
+                        </Card>
+                </div>
             </div>
             <div className="grid">
                 <Button className="view-all" variant="dark" href="/Destinations">View our locations</Button>
