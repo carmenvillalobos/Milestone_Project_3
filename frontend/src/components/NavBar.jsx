606e713017254d71b58b04eb519387323a1e7486
Sierra Lopez <sierralopez1006@gmail.com>
2023-04-06 15:54:45 -0700

    fixed navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,29 +1,32 @@
 import Navbar from 'react-bootstrap/Nav'
+import Image from 'react-bootstrap/Image'
 import '../assets/css/Homepage.css'
 
 function NavBar() {
     return(
-        <Navbar className="nav justify-content-space-between sticky-top">
-            <Navbar.Item className="nav">
-                <Navbar.Link className='text home' href='/'>Home</Navbar.Link>
-            </Navbar.Item>
-            <Navbar.Item className="nav">
-                <Navbar.Link className='text' href='/Bookings'>My Trips</Navbar.Link>
-            </Navbar.Item>
-            {/* <Navbar.Item className="nav">
-                <Navbar.Link className='text' href='/Destinations'>Favorite Places</Navbar.Link>
-            </Navbar.Item> */}
-            <Navbar.Item className="nav">
-                <Navbar.Link className='text' href='/Destinations'>All Destinations</Navbar.Link>
-            </Navbar.Item>
-            {/* Log-in */}
-            <Navbar.Item className="nav">
-                <Navbar.Link className='text personal' href='/Login'>Login</Navbar.Link>
-            </Navbar.Item>
-            {/* Sign-up */}
-            <Navbar.Item className="nav">
-                <Navbar.Link className='text personal' href='/SignUp'>Sign-up</Navbar.Link>
-            </Navbar.Item>
+        <Navbar className="justify-content-space-between sticky-top">
+            <div className="nav logo">
+                <Navbar.Item><Image src="https://image.similarpng.com/very-thumbnail/2021/08/Illustration-of-Travel-agency-logo-design-template-on-transparent-background-PNG.png" alt="logo" width="50" height="50" roundedCircle/></Navbar.Item>
+            </div>
+            <div className="nav home">
+                <Navbar.Item>
+                    <Navbar.Link className='text' href='/'>Home</Navbar.Link>
+                </Navbar.Item>
+                <Navbar.Item>
+                    <Navbar.Link className='text' href='/Bookings'>My Trips</Navbar.Link>
+                </Navbar.Item>
+                <Navbar.Item>
+                    <Navbar.Link className='text' href='/Destinations'>All Destinations</Navbar.Link>
+                </Navbar.Item>
+            </div>
+            <div className="nav personal">
+                <Navbar.Item>
+                    <Navbar.Link className='text' href='/Login'>Login</Navbar.Link>
+                </Navbar.Item>
+                <Navbar.Item>
+                    <Navbar.Link className='text' href='/SignUp'>Sign-up</Navbar.Link>
+                </Navbar.Item>
+            </div>
         </Navbar>
     )
 }
