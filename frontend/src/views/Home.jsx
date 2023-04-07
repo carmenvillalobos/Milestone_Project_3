52a4cfb5f15cbe9918c705c94dd8240b67abd81e
Sierra Lopez <sierralopez1006@gmail.com>
2023-04-06 20:22:58 -0700

    media queries

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -11,6 +11,7 @@ function Homepage (props){
     return(
         <div className='main'>
             <NavBar/>
+            
             <div>
                 <img className="home-img" src="https://images.unsplash.com/photo-1488646953014-85cb44e25828?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80" alt="travel"/>
                 <SearchBar/>
