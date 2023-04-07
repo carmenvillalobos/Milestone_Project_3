017585436397cce21a9bf01863d05a51221e47eb
Sierra Lopez <sierralopez1006@gmail.com>
2023-04-06 17:08:54 -0700

    success popup on reservation

--- frontend/src/views/Destination.jsx
+++ frontend/src/views/Destination.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router"
 import '../assets/css/Destination.css'
 import Button from "react-bootstrap/Button"
+import Modal from "react-bootstrap/Modal"
 import NavBar from "../components/NavBar"
 
 function Destination(props) {
@@ -9,6 +10,10 @@ function Destination(props) {
     const { id } = useParams()
 
 	const [destination, setDestination] = useState({})
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     useEffect(() => {
 		const fetchData = async () => {
@@ -52,8 +57,18 @@ function Destination(props) {
                 <p style={{color: "white", border: "2px solid", borderRadius: "5px"}}>$362 /night</p>
                 {/* <p style={{color: "white", border: "2px solid", borderRadius: "5px"}}>{destination.price}</p> */}
                 <p style={{color: "white", border: "2px solid", borderRadius: "5px"}}>{isAvailable(`${destination.booking_secured}`)}</p>
-                <Button className="reserve border col-sm-3" variant="dark" href="/Success">Reserve</Button>
+                <Button className="reserve border col-sm-3" variant="dark" onClick={handleShow}>Reserve</Button>
             </div>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Woohoo!</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Congrats and pack your bags! You're going to {destination.location}</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>Close</Button>
+                    <Button variant="primary" onClick={handleClose} href="/Bookings">See all reservations</Button>
+                </Modal.Footer>
+            </Modal>
             
         </div>
     )
